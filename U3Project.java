f2616c10f9d97a360120158adc457a1d72c462d7
Kaden70 <kaden.hays2007@gmail.com>
2026-03-02 09:10:49 -0600

    Update U3Project.java

--- U3Project.java
+++ U3Project.java
@@ -4,24 +4,45 @@ import java.util.Scanner;
 public class U3Project {
     public static void main(String args[]) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter a number between 1-1000 and the computer will guess it. Respond with: L means it guess to low. Type H if it guessed to high. Press G if the computer guessed your number.");
+        System.out.println("Enter a number between 1-1000 and the computer will guess it. Respond with: Type L if your number is lower. Type H if your number is higher. Type G if the computer guessed your number. Make sure numbers are uppercase.");
         int number = input.nextInt();
-        input.close();
 
         int guess = 500;
 
-        while (guess != number && number > 0 && number < 1001) {
-            if (guess == number) {
-                for (int w = 0; w < number; w++) {
-                    System.out.println("hip-hip-hooray ");
-                    if (w == number) {
-                        break;
-                    }
-                }
+        while (guess != number) {
             System.out.println("Is your number " + guess + "?");
-            } else {
+            char feedback = input.next().charAt(0);
+            if (feedback == 'L') {
+                guess = guess / 2;
+            } else if (feedback == 'H') {
+                guess = guess + (guess / 2);
+            } else if (feedback == 'G') {
+                for (int w = 1; w < number + 1; w++) {
+                    System.out.println(w + " hip-hip-hooray!");
+                }
                 break;
             }
         }
+        input.close();
     }
 }
+
+
+
+
+//while (guess != number && number > 0 && number < 1001) {
+//            if (guess == number) {
+//                for (int w = 0; w < number; w++) {
+//                    System.out.println("hip-hip-hooray");
+//                    if (w == number) {
+//                        break;
+//                    }
+//                }
+//            } else if (guess != number) {
+//                System.out.println("Is your number " + guess + "?");
+//                if (guess != number) {
+//                    number = number / 2;
+//            } else {
+//                break;
+//            }
+//        }
